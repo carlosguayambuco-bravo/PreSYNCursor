cdc3ed5d58a3e299979dfc04b9398b58cb0f888c
carlosguayambuco-gobravo <carlos.guayambuco@gobravo.com.co>
2026-08-03 11:11:18 -0500

    Added very solid login page

--- ui/login_components.py
+++ ui/login_components.py
@@ -8,3 +8,32 @@ from core.users import User
 def show_user_info():
     # Primero Cargamos la Información del Usuario desde el estado de sesión
     user: User = st.session_state['user_obj']
+
+    # Ahora vamos a Poner en el Sidebar:
+    # Circulo Avatar con la Inicial del Nombre del Usuario
+    st.sidebar.markdown(
+        f"""
+        <div style="display: flex; flex-direction: column; align-items: center; margin-bottom: 10px;">
+            <div style="width: 40px; height: 40px; border-radius: 50%; background-color: #4CAF50; color: white; display: flex; align-items: center; justify-content: center; font-weight: bold; font-size: 18px;">
+                {user.name[0].upper()}
+            </div>
+            <div style="margin-top: 8px; text-align: center;">
+                <strong>{user.name}</strong><br>
+                <span style="font-size: 12px; font-weight: normal; font-style: italic;">
+                    {user.email}
+                </span>
+            </div>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+    # Ahora añadimos Botón de Cerrar Sesión en el Sidebar
+    if st.sidebar.button("Cerrar Sesión", icon="🚪"):
+        # Limpiamos el estado de sesión
+        st.session_state.clear()
+        # Redirigimos al usuario a la página de login
+        st.rerun()
+
+    # Añadimos un Divisor al Sidebar para que no interfiera con las páginas de la aplicación
+    st.sidebar.divider()
--- views/login_page.py
+++ views/login_page.py
@@ -5,12 +5,12 @@ import streamlit as st
 # Librerías Locales
 from core.auth import get_auth_url
 
-def show_login_page():
-    st.title("Bienvenido a la Aplicación de Gestión de Formularios")
-    st.write("Por favor, inicia sesión con tu cuenta de Google para continuar.")
-    st.write("Si no tienes una cuenta, por favor contacta al administrador.")
+st.title("😁Bienvenido a la Aplicación de Gestión de Alianzas")
+st.write("🔏Por favor, inicia sesión con tu cuenta de Google para continuar.")
+st.write("Si no tienes una cuenta, por favor contacta al administrador.")
+st.divider()
 
-    # Botón para iniciar sesión con Google
-    if st.button("Iniciar sesión con Google"):
-        auth_url = get_auth_url()
-        st.write(f"[Haz clic aquí para iniciar sesión]({auth_url})")
+# Botón para iniciar sesión con Google
+link_url = get_auth_url()
+st.link_button("Iniciar sesión con Google", link_url, icon='🗒️', width="stretch")
+st.stop()  # Detenemos la ejecución del script hasta que el usuario se autentique
