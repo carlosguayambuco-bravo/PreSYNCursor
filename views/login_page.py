f96f488e7f3c25f09de4b91b0d64feadf0ed145e
carlosguayambuco-gobravo <carlos.guayambuco@gobravo.com.co>
2026-08-12 14:28:27 -0500

    Found error, requires new page

--- views/login_page.py
+++ views/login_page.py
@@ -22,38 +22,7 @@ def show_login_page():
     else:
         auth_url = st.session_state["auth_url"]
 
-    st.markdown(f"({auth_url})")
-
-    st.markdown(
-    f"""
-        <a href="{auth_url}" target="_self" style="text-decoration: none; display: block; width: 100%;">
-            <button style="
-                width: 100%;
-                background-color: #FF4B4B;
-                color: white;
-                border: none;
-                padding: 0.5rem 1rem;
-                font-size: 1rem;
-                font-weight: 500;
-                border-radius: 0.5rem;
-                cursor: pointer;
-                transition: background-color 0.2s ease, border-color 0.2s ease;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                box-sizing: border-box;
-                line-height: 1.6;
-                font-family: Source Sans Pro, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif;
-            "
-            onmouseover="this.style.backgroundColor='#FF2B2B';"
-            onmouseout="this.style.backgroundColor='#FF4B4B';"
-            >
-                🤐 Iniciar Sesión con Google
-            </button>
-        </a>
-        """,
-        unsafe_allow_html=True,
-    )
+    st.link_button("Iniciar sesión con Google", url=auth_url, type="primary", width="stretch", icon="🔏")
 
     st.space("medium")
     # Creamos un Botón para volver a generar el enlace de autenticación en caso de que el usuario no pueda iniciar sesión
