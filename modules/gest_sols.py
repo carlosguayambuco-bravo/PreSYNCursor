56910abb34f752838616a5417af8dd8674845d27
carlosguayambuco-gobravo <carlos.guayambuco@gobravo.com.co>
2026-08-13 17:14:51 -0500

    Fixed error of Strftime

--- modules/gest_sols.py
+++ modules/gest_sols.py
@@ -760,8 +760,8 @@ def generate_plantilla_serie_acuerdo(*, solicitud: pd.Series, deudas: list[str])
             "Metodo_Pago": solicitud['Metadata_Solicitud'].get('Metodo_Pago', ''),
             "Comentario_Ejecutivo": solicitud['Metadata_Solicitud'].get('Comentario_Ejecutivo', ''),
         },
-        "Fecha_Esperada_Pago": solicitud['Fecha_Esperada_Pago'].strftime("%Y-%m-%d") if pd.notna(solicitud['Fecha_Esperada_Pago']) else '',
-        "Fecha_Limite_Pago": solicitud['Fecha_Limite_Pago'].strftime("%Y-%m-%d"),
+        "Fecha_Esperada_Pago": pd.to_datetime(solicitud['Fecha_Esperada_Pago']).strftime("%Y-%m-%d") if pd.notna(solicitud['Fecha_Esperada_Pago']) else '',
+        "Fecha_Limite_Pago": pd.to_datetime(solicitud['Fecha_Limite_Pago']).strftime("%Y-%m-%d"),
         "Casa_Cobro": solicitud['Casa_Cobro'],
         "Ejecutivo": solicitud['Ejecutivo'],
         "JSON_Respuesta": [
