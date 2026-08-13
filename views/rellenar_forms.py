2e04f577c6508b8d9719f2768327eab7c47a92a1
carlosguayambuco-gobravo <carlos.guayambuco@gobravo.com.co>
2026-08-13 15:47:28 -0500

    Solved issue of Auto-Falsing

--- views/rellenar_forms.py
+++ views/rellenar_forms.py
@@ -108,6 +108,10 @@ ultima_actualizacion = obtener_ultima_actualizacion_deudas(debt_ids=deudas_activ
 cumple_condicion, dias_habiles_diff = cumple_condicion_actualizacion_deudas(ultima_actualizacion=ultima_actualizacion)
 user = st.session_state['user_obj']
 es_admin = (user.role == 'admin')
+
+if es_admin:
+    st.write(f"{cumple_condicion =} {st.secrets.get('LET_WITHOUT_UPDATE', False) =}")
+
 st.info('ℹ️Última Actualización de las Deudas Activas: {} (Hace {:.2f} días hábiles)'.format(
     ultima_actualizacion.strftime('%Y-%m-%d') if ultima_actualizacion else 'No Disponible',
     dias_habiles_diff,
