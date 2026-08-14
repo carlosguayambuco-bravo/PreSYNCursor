3b26f309244931fe2e7b7994f37aa13b10e62de9
carlosguayambuco-gobravo <carlos.guayambuco@gobravo.com.co>
2026-08-14 11:46:40 -0500

    Solved Duplicated Debts Issue

--- modules/constants.py
+++ modules/constants.py
@@ -65,11 +65,11 @@ WITH PLvanex AS (
     SELECT
         cr.bank_reference,
         cr.id AS cr_id,
-        vsp.success_commission_percentage as Pricing,
+        vsp.success_commission_percentage AS Pricing,
         JSON_EXTRACT_SCALAR(debt_item, '$.financial_entity') AS financial_entity,
         CAST(JSON_EXTRACT_SCALAR(debt_item, '$.updated_amount') AS FLOAT64) AS debt_original_amount,
         CAST(JSON_EXTRACT_SCALAR(debt_item, '$.payment_to_bank') AS FLOAT64)
-        + CAST(JSON_EXTRACT_SCALAR(debt_item, '$.reduction_commission') AS FLOAT64) AS PB_PL
+            + CAST(JSON_EXTRACT_SCALAR(debt_item, '$.reduction_commission') AS FLOAT64) AS PB_PL
     FROM vanex_public.settlement_plan AS vsp
     LEFT JOIN UNNEST(JSON_EXTRACT_ARRAY(vsp.debts)) AS debt_item
     LEFT JOIN vanex_public.leads_lead AS ll
@@ -80,7 +80,6 @@ WITH PLvanex AS (
         AND vsp.winner IS TRUE
 ),
 
--- 3) Intento mapear ese plan a debt_id
 PL_Programa AS (
     SELECT
         ids.id AS debt_id,
@@ -89,18 +88,18 @@ PL_Programa AS (
     FROM PLvanex v
     LEFT JOIN (
         SELECT
-        crd.id,
-        CAST(crd.amount AS FLOAT64) AS monto,
-        crd.financial_entity_name,
-        crd.credit_repair_id
+            crd.id,
+            CAST(crd.amount AS FLOAT64) AS monto,
+            crd.financial_entity_name,
+            crd.credit_repair_id
         FROM dealer_public.berex_credit_repair_debts AS crd
         LEFT JOIN berex_public.credit_repairs AS cr
-        ON cr.id = crd.credit_repair_id
+            ON cr.id = crd.credit_repair_id
         WHERE cr.country = 'co'
     ) AS ids
         ON v.cr_id = ids.credit_repair_id
-    AND v.financial_entity = ids.financial_entity_name
-    AND v.debt_original_amount = ids.monto
+        AND v.financial_entity = ids.financial_entity_name
+        AND v.debt_original_amount = ids.monto
 )
 
 SELECT
@@ -113,20 +112,41 @@ SELECT
     bcrd.amount AS PaB_Origen,
     pl.PB_PL AS PaB_PL,
     vsp.success_commission_percentage AS Pricing
+
 FROM dealer_public.berex_credit_repair_debts bcrd
-    LEFT JOIN dealer_public.berex_credit_repairs AS bcr
-        ON bcr.id = bcrd.credit_repair_id
-    LEFT JOIN PL_Programa AS pl
-        ON bcrd.id = pl.debt_id
-    LEFT JOIN vanex_public.leads_lead AS ll
-        ON ll.tracker_id = bcr.tracker_id
-    LEFT JOIN vanex_public.settlement_plan AS vsp
-        ON vsp.lead_id = ll.id
+
+LEFT JOIN dealer_public.berex_credit_repairs AS bcr
+    ON bcr.id = bcrd.credit_repair_id
+
+LEFT JOIN PL_Programa AS pl
+    ON bcrd.id = pl.debt_id
+
+LEFT JOIN vanex_public.leads_lead AS ll
+    ON ll.tracker_id = bcr.tracker_id
+
+LEFT JOIN vanex_public.settlement_plan AS vsp
+    ON vsp.lead_id = ll.id
+
 WHERE 
-    bcrd.state IN ('new','negotiation','lawsuit')
-    AND NOT(bcrd.sub_state IN ('liquidated','liquidated_with_credit','liquidation_in_process','cancelled','drop_requested','liquidation_structured_payment'))
+    bcrd.state IN ('new', 'negotiation', 'lawsuit')
+    AND NOT (
+        bcrd.sub_state IN (
+            'liquidated',
+            'liquidated_with_credit',
+            'liquidation_in_process',
+            'cancelled',
+            'drop_requested',
+            'liquidation_structured_payment'
+        )
+    )
     AND vsp.winner IS TRUE
-    AND bcrd.bank_reference = '{referencia}';
+    AND bcrd.bank_reference = '{referencia}'
+
+QUALIFY 
+    ROW_NUMBER() OVER (
+        PARTITION BY bcrd.id
+        ORDER BY bcrd.updated_at DESC
+    ) = 1;
 """
 
 QUERY_LAST_UPDATE = """
